5c9071b48bbb943555647aaaed7869165f01e29c
Matthew Durocher <matthewdurocher@Matthews-Mac-Studio.local>
2022-08-31 13:47:43 -0700

    adding files after issues earlier this week with the old repository

---
+++ Python_practice.py
@@ -0,0 +1,3 @@
+my_votes = int(input("How many votes did you get in the elction? "))
+total_votes = int(input("How many people voted in the elction? "))
+print(f"I recieved {my_votes/total_votes * 100:.2f}% of the total votes")
